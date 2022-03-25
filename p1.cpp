9f5cfca10d5665215540bdfff5fdd7370b62c9b4
Prober76 <ugartegrover@gmail.com>
2022-03-25 17:23:18 -0500

    p1 terminado

---
+++ p1.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Grove on 25/03/2022.
+//
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+using namespace std;
+
+struct Alumno{
+    Alumno(){}
+    Alumno(string _codigo, string _nombre, string _apellidos , string _carrera){
+        for(int i = 0; i<5; i++){
+            if(i < _codigo.size()) codigo[i] = _codigo[i];
+            else codigo[i] = ' ';
+        }
+        for(int i = 0; i<11; i++){
+            if(i < _nombre.size()) nombre[i] = _nombre[i];
+            else nombre[i] = ' ';
+        }
+        for(int i = 0; i<20; i++){
+            if(i < _apellidos.size()) apellidos[i] = _apellidos[i];
+            else apellidos[i] = ' ';
+        }
+        for(int i = 0; i<15; i++){
+            if(i < _carrera.size()) carrera[i] = _carrera[i];
+            else carrera[i] = ' ';
+        }
+    }
+    char codigo [5];
+    char nombre [11];
+    char apellidos [20];
+    char carrera [15];
+};
+
+ostream& operator<<(ostream& out, Alumno& a){
+    out.write(a.codigo, sizeof(Alumno::codigo));
+    out.write(a.nombre, sizeof(Alumno::nombre));
+    out.write(a.apellidos, sizeof(Alumno::apellidos));
+    out.write(a.carrera, sizeof(Alumno::carrera));
+    out.put('\n');
+    out.flush();
+    return out;
+}
+
+istream& operator>>(istream& is, Alumno& a){
+    is.read(a.codigo, sizeof(Alumno::codigo));
+    is.read(a.nombre, sizeof(Alumno::nombre));
+    is.read(a.apellidos, sizeof(Alumno::apellidos));
+    is.read(a.carrera, sizeof(Alumno::carrera));
+    is.get();
+    return is;
+}
+
+class FixedRecord{
+public:
+    FixedRecord(string _path): path(_path){
+        ofstream archivo;
+        archivo.open(path, ios::out|ios::app);
+        archivo.close();
+    }
+    vector<Alumno> load(){
+        ifstream archivo;
+        archivo.open(path, ios::in);
+        vector<Alumno> result;
+        while(archivo.peek() != EOF){
+            Alumno temp;
+            archivo>>temp;
+            result.push_back(temp);
+        }
+        archivo.close();
+        return result;
+    }
+    void add(Alumno a){
+        ofstream archivo;
+        archivo.open(path, ios::app);
+        archivo<<a;
+        archivo.close();
+    }
+    Alumno readRecord(int pos){
+        ifstream archivo;
+        archivo.open(path, ios::in);
+        for (int i = 0; i < pos; i++){
+            archivo.ignore(sizeof(Alumno)+1);
+        }
+        if(archivo.peek() == EOF) throw("No existe el registro");
+        Alumno result;
+        archivo>>result;
+        archivo.close();
+
+        return result;
+    }
+private:
+    string path;
+};
+
+int main(){
+
+    try{
+        auto f = FixedRecord("datos1.txt");
+        // IMPRIMIR TODOS LOS REGISTRO
+        for(auto i: f.load()) cout<<i;
+        // AGREGAR REGISTRO
+        Alumno a1{"0009", "Facundo", "Gimenez Velasco", "Informatica"};
+        f.add(a1);
+        // LEER REGISTRO 5
+        auto a2 = f.readRecord(4);
+        cout<<a2;
+    }
+    catch(const char* a){
+        cout<<a<<endl;
+    }
+    
+    return 0;
+}
